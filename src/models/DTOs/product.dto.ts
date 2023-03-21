72b1c1ec08530d6383fc2e18892cb644872ad803
PabloGCH <pablochoconi@gmail.com>
2023-03-20 22:41:24 -0300

    sqlite fixes in products CRUD

--- src/models/DTOs/product.dto.ts
+++ src/models/DTOs/product.dto.ts
@@ -1,24 +1,29 @@
+import { errorLogger } from "../../services/logger.service";
+
 export class ProductDTO {
-    id: string;
-    name: string;
-    price: number;
-    imgURL: string;
-    description: string;
-    category: string;
+    id: string = "";
+    name: string = "";
+    price: string = "";
+    imgURL: string = "";
+    description: string = "";
+    category: string = "";
     constructor(object :any) {
-        console.log("ProductDTO: constructor", object);
-        if (!object) { throw "ProductDTO: object is null or undefined";}
-        if (!object.id && !object._id) { throw "ProductDTO: object.id is null or undefined";}
-        if (!object.name) { throw "ProductDTO: object.name is null or undefined";}
-        if (!object.price) { throw "ProductDTO: object.price is null or undefined";}
-        if (!object.imgURL) { throw "ProductDTO: object.imgURL is null or undefined";}
-        if (!object.description) { throw "ProductDTO: object.description is null or undefined";}
-        if (!object.category) { throw "ProductDTO: object.category is null or undefined";}
-        this.id = object.id || object._id;
-        this.name = object.name;
-        this.price = object.price;
-        this.imgURL = object.imgURL;
-        this.description = object.description;
-        this.category = object.category;
+        try {
+            if (!object) { throw "ProductDTO: object is null or undefined";}
+            if (!object.id && !object._id) { throw "ProductDTO: object.id is null or undefined";}
+            if (!object.name) { throw "ProductDTO: object.name is null or undefined";}
+            if (!object.price) { throw "ProductDTO: object.price is null or undefined";}
+            if (!object.imgURL) { throw "ProductDTO: object.imgURL is null or undefined";}
+            if (!object.description) { throw "ProductDTO: object.description is null or undefined";}
+            if (!object.category) { throw "ProductDTO: object.category is null or undefined";}
+            this.id = object.id || object._id;
+            this.name = object.name;
+            this.price = object.price;
+            this.imgURL = object.imgURL;
+            this.description = object.description;
+            this.category = object.category;
+        } catch (error) {
+            errorLogger.error(error);
+        }
     }
 }
--- src/persistence/clients/sql.client.ts
+++ src/persistence/clients/sql.client.ts
@@ -1,6 +1,7 @@
 import { DbClient } from "../dbclient";
 import { Knex } from "knex";
 import { SQLDatabaseConnection } from "../config/knex.config";
+import { errorLogger } from "../../services/logger.service";
 
 
 
@@ -12,25 +13,57 @@ export class SQLClient implements DbClient {
         this.database = SQLDatabaseConnection.getInstance().getDatabase();
     }
     public async save(object: any): Promise<any> {
-        let newObjectID = await this.database(this.tableName).insert(object);
-        let newObject = await this.database(this.tableName).first({id: newObjectID});
-        return newObject;
+        try {
+            let newObjectID = await this.database(this.tableName).insert(object);
+            let newObject = await this.database(this.tableName).where({id: newObjectID[0]}).first();
+            return newObject;
+        } catch (error) {
+            errorLogger.error(error);
+            return null;
+        }
     }
     public async update(id: string | number, object: any): Promise<any> {
-        let newObjectID = await this.database(this.tableName).first({id: id}).update(object);
-        let newObject = await this.database(this.tableName).first({id: newObjectID});
-        return newObject;
+        try {
+            let newObjectID :any = await this.database(this.tableName).where({id: id}).update(object);
+            console.log("OBJECT ID", newObjectID);
+            let newObject = await this.database(this.tableName).where({id: newObjectID}).first();
+            console.log("OBJECT", newObject);
+            return newObject;
+        } catch (error) {
+            errorLogger.error(error);
+            return null;
+        }
     }
     public async delete(id: number): Promise<any> {
-        let object = await this.database(this.tableName).where({id: id}).del();
-        return object;
+        try {
+            let deletedObject = await this.database(this.tableName).where({id: id}).first();
+            if (deletedObject) {
+                await this.database(this.tableName).where({id: id}).first().del();
+                return deletedObject;
+            } else {
+                return null;
+            }
+        } catch (error) {
+            errorLogger.error(error);
+            return null;
+        }
     }
     public async getObjects(): Promise<any[]> {
-        let objects = await this.database(this.tableName).select();
-        return objects;
+        try {
+            let objects = await this.database(this.tableName).select();
+            return objects;
+        } catch (error) {
+            errorLogger.error(error);
+            return [];
+        }
     }
     public async getObject(id: string | number): Promise<any> {
-        let object = await this.database(this.tableName).first({id: id});
-        return object;
+        try {
+            let object = await this.database(this.tableName).where({id: id}).first();
+            return object;
+        } catch (error) {
+            errorLogger.error(error);
+            return null;
+        }
     }
 }
